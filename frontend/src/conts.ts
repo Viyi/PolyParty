cb776372b82d1639b0899fee287e522449b5a499
Liam McBride <mailmcbride56@gmail.com>
2026-02-08 00:08:56 +0000

    feat: added in frontend use of HOST value which is mapped to VITE_API_URL: needs rename

--- frontend/src/conts.ts
+++ frontend/src/conts.ts
@@ -1,5 +1,5 @@
 import { browser } from '$app/environment';
 
 export const API_HOST = browser
-	? `${window.location.protocol}//localhost:8051`
+	? `${window.location.protocol}//${import.meta.env.VITE_API_URL}:8051`
 	: 'http://localhost:8051'; // Fallback for server-side rendering
